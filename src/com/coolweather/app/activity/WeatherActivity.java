b71a669ab1f713ea6d88df24cd1eba6a7a1b87f9
zhangchen <chzhang_knowology@163.com>
2017-03-12 17:36:41 +0800

    add the function of auto update weather report.

--- src/com/coolweather/app/activity/WeatherActivity.java
+++ src/com/coolweather/app/activity/WeatherActivity.java
@@ -1,6 +1,7 @@
 package com.coolweather.app.activity;
 
 import com.coolweather.app.R;
+import com.coolweather.app.service.AutoUpdateService;
 import com.coolweather.app.util.HttpCallbackListener;
 import com.coolweather.app.util.HttpUtil;
 import com.coolweather.app.util.Utility;
@@ -161,5 +162,7 @@ public class WeatherActivity extends Activity implements OnClickListener{
 		currentDateText.setText(pref.getString("current_time", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
+		Intent i = new Intent(this, AutoUpdateService.class);
+		startService(i);
 	}
 }
